65932306209870dd3d8e9358ef600f5b993da97a
unknown <shobhitsundriyal0170@gmail.com>
2020-01-18 22:44:00 +0530

    hmm

--- 1. Breakout/app.py
+++ 1. Breakout/app.py
@@ -2,4 +2,8 @@ import os
 import numpy as np
 import tensorflow as tf 
 
-class DeepQNetwork(self, lr, n_actions,)
+class DeepQNetwork(object):
+    def __init__(self, lr, n_actions, name, fcl_dims=256, input_dims=(210,160),
+                chkpt_dir='/tmp'):
+        self.lr = lr
+        
