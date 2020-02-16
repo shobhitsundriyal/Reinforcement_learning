2fd0cab9d6df7397160ead7a755132df47c3488f
Shobhit Sundriyal <35799284+shobhitsundriyal@users.noreply.github.com>
2020-02-16 23:19:14 +0530

    Update app.py

--- 1. Breakout/app.py
+++ 1. Breakout/app.py
@@ -6,4 +6,5 @@ class DeepQNetwork(object):
     def __init__(self, lr, n_actions, name, fcl_dims=256, input_dims=(210,160),
                 chkpt_dir='/tmp'):
         self.lr = lr
+        self.
         
