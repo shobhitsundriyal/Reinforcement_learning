65823c3ab7f861b67d97bcc50e57c55d118de4f6
unknown <shobhitsundriyal0170@gmail.com>
2020-02-18 17:30:44 +0530

    done..

--- Mountain_carQL.py
+++ Mountain_carQL.py
@@ -1,17 +1,73 @@
 import gym
+import numpy as np 
 
 env = gym.make('MountainCar-v0')
 env.reset()
 
-done = False
+#print(env.observation_space.low)
+#print(env.action_space.n) Add T3 for metricsoutput
+#print(env.observation_space.high)
 
-while not done:
-    ''' action 0 ==> go  left
-        action 1 ==> do nothing
-        action 2 ==> go right
-    '''
-    action = 2
-    new_state, reward, done, _ = env.step(action)
-    env.render()
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPISODES = 25000
+
+DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+#print(discrete_os_win_size)
+
+epsilon = 0.5
+START_EPSILON_DECAY = 1
+END_EPSILON_DECAY = EPISODES // 2
+epsilon_decay_value = epsilon / (END_EPSILON_DECAY - START_EPSILON_DECAY)
+
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+#print(q_table.shape)
+#print(q_table)
+
+def get_discrete_state(state):
+    discrete_state = (state - env.observation_space.low) / discrete_os_win_size
+    return tuple(discrete_state.astype(np.int))
+
+
+for episode in range(EPISODES):
+    if episode % 2000 == 0:
+        print(episode)
+        render = True
+    else:
+        render = False
+    discrete_state = get_discrete_state(env.reset())
+
+    done = False
+
+    while not done:
+        ''' action 0 ==> go  left
+            action 1 ==> do nothing
+            action 2 ==> go right
+        '''
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[discrete_state])
+        else:
+            action = np.random.randint(0, env.action_space.n)
+        new_state, reward, done, _ = env.step(action)
+        new_discrete_state = get_discrete_state(new_state)
+
+        if render:
+            env.render()
+
+        if not done:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action, )]
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            q_table[discrete_state + (action, )] = new_q
+
+        elif new_state[0] >= env.goal_position:
+            print('Done at episode:',episode)
+            q_table[discrete_state + (action, )] = 0
+
+        discrete_state = new_discrete_state
+
+    if END_EPSILON_DECAY >= episode >= START_EPSILON_DECAY:
+        epsilon -= epsilon_decay_value 
 
 env.close()
